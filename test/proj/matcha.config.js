f98dd8d205393e3d03956180e79e4f2fda19b173
yichenyang <yichenyang@tencent.com>
2020-11-11 10:20:56 +0800

    feat: 增加命令行入口

---
+++ test/proj/matcha.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  port: 3004,
+  env: 'fat',
+  mode: 'development',
+  root: __dirname,
+  src: './src',
+  out: './out',
+  publish: './publish',
+  publicPath: '/app',
+  bundleName: 'server.bundle.js',
+  staticPath: '/static/',
+}
